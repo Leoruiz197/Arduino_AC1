6d99694c544a61336741ed58f3347660eb0261b2
carolseiffarth <78746548+carolseiffarth@users.noreply.github.com>
2021-03-31 15:33:07 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -1,66 +1,13 @@
 //variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
+const int azul = 7;
 
-bool estadoLedVermelho = false;
+bool estadoLedVermelho = true;
 
 const int botao1 = 2;
 unsigned long lastDebounceTime1 = 0;
 const int botaoDelay = 100;
 
-void setup()
-{
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  
-  pinMode(vermelho, OUTPUT);
-  pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
-  
-  Serial.begin(9600);
-	
-  Serial.println("AC1 - Meu Primeiro Projeto 2021");
-  Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
-}
-
-void loop()
-{
-  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("botao 1 apertado");
-    ledVermelho();
-  	lastDebounceTime1 = millis();
-  }
-  
-  if(getTemperatura() > 30){
-    ledAzul(true);
-  }else{
-  	ledAzul(falso); 
-  }
-  	
-  delay(10);
-}
-
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
-}
-void ledVerde(){
- //precisa fazer  
-}
-void ledAzul(bool estado){
-	digitalWrite(azul,estado);
-}
-
-int getTemperatura(){
-  	int temperaturaC;
-	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-  	return temperaturaC;
-} 
-//funcao de leitura da luminosidade
-int getLuminosidade(){
-  	int luminosidade;
-	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
-  	return luminosidade;
-}
+const int botao2 = 3;
+unsigned long lastDebounceTime2 = 0;
